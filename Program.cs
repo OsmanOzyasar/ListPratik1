4dc1e0a2cbe2f1044766b2e9c328926e7373ad96
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-15 15:21:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace List1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> davetliler = new List<string>() {"Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise","Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın"}; //DAVETLİLERİN OLDUĞU BİR STRİNG LİSTE OLUŞTURULMUŞTUR
+
+            int number = 1;
+
+            Console.WriteLine("**DAVETLİLER**");
+
+            foreach (string s in davetliler) //foreach döngüsü ile hepsi ekrana yazdırılır
+            {
+                Console.WriteLine($"{number} - {s}");
+                number++;
+            }
+        }
+    }
+}
